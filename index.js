37556f3f79c592942d51f44f0f6e548cca5c0892
mono motapa <bahobab@hotmail.com>
2019-07-10 12:36:29 -0500

    implemented passort authentication with passport-googleo-auth20

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStarategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 
 const app = express();
+
+passport.use(new GoogleStarategy({
+    clientID: keys.GOOGLE_CLIENT_ID,
+    clientSecret: keys.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    // console.log(refreshToken);
+    console.log(profile);
+
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('auth/google/callback', passport.authenticate('google'));
 
 app.get('/', (req, res) => {
     res.send({greeting: 'Salut les copains!...'});
