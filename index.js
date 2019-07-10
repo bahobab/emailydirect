fd38991c5defd89dfc3bcc1f1899287e7d7fda59
mono motapa <bahobab@hotmail.com>
2019-07-10 15:32:53 -0500

    setup mongodb database on mLab. Created users collection with un user doc

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
+
+require('./models/User');
 require('./services/passport'); // just the file is required
+
 const app = express();
 
 require('./routes/google.auth.route')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,21 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); // this is the 'users' model we created
+// in User.js
 
 passport.use(new GoogleStrategy({
     clientID: keys.GOOGLE_CLIENT_ID,
     clientSecret: keys.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    // console.log(refreshToken);
-    console.log(profile);
-
+}, async(accessToken, refreshToken, profile, done) => {
+    try {
+        const newUser = await new User({googleId: profile.id}).save();
+        console.log(newUser);
+    } catch (error) {
+        console.log(error);
+    }
 }));
