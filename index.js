4fbbce84f5d42362b4518945ce1d65c262881dca
mono motapa <bahobab@hotmail.com>
2019-07-10 14:28:23 -0500

    fixed typo in google.auth.route.js and passport.js

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStarategy = require('passport-google-oauth20').Strategy;
 
-const keys = require('./config/keys');
-
+require('./services/passport'); // just the file is required
 const app = express();
 
-passport.use(new GoogleStarategy({
-    clientID: keys.GOOGLE_CLIENT_ID,
-    clientSecret: keys.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    // console.log(refreshToken);
-    console.log(profile);
-
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('auth/google/callback', passport.authenticate('google'));
+require('./routes/google.auth.route')(app);
 
 app.get('/', (req, res) => {
     res.send({greeting: 'Salut les copains!...'});
