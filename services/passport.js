4e4b2674ea191812f60839df3c4b9a1686aea68d
mono motapa <bahobab@hotmail.com>
2019-07-10 22:17:49 -0500

    tweaked google strategy by setting proxy: true

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.deserializeUser(async(id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.GOOGLE_CLIENT_ID,
     clientSecret: keys.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, async(accessToken, refreshToken, profile, done) => {
     try {
         const existingUser = await User.findOne({googleId: profile.id});
