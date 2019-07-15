d942d2b8f05db4ef4d3b412806b2e152670acdda
mono motapa <bahobab@hotmail.com>
2019-07-15 14:36:40 -0500

    Handled redurect after user logged out

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -20,7 +20,6 @@ class Header extends React.Component {
         }
     }
     render() {
-        console.log(this.props.auth);
         return (
             <nav>
                 <div className="nav-wrapper">
