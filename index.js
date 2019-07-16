a8702d35600314746e4c03622c04e98848a428fc
mono motapa <bahobab@hotmail.com>
2019-07-16 11:08:04 -0500

    Added routing code for production in server/index.js. Added client build script in server/package.jsom

--- index.js
+++ index.js
@@ -28,9 +28,18 @@ app.use(passport.session());
 require("./routes/google.auth.route")(app);
 require("./routes/billringRoutes")(app);
 
-app.get("/", (req, res) => {
-  res.send({ greeting: "Salut les copains!..." });
-});
+if (process.env.NODE_ENV === "production") {
+  // Express will server up production assets
+  // like our main.js, main.css files
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
