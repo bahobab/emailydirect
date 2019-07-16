4eabb797a092de4627d2e25ec0f74fb63631ec22
mono motapa <bahobab@hotmail.com>
2019-07-15 21:07:03 -0500

    Preparing for implementation of charge with stripe: insstalled stripe prosessing package and express middlewares: helmet and body-parser

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const mongoose = require('mongoose');
+const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
 
-const keys = require('./config/keys');
+const keys = require("./config/keys");
 mongoose.connect(keys.MONGO_URI);
 
-require('./models/User');
-require('./services/passport'); // just the file is required
+require("./models/User");
+require("./services/passport"); // just the file is required
 
 const app = express();
 
-app.use(cookieSession({
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.COOKIE_KEY]
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/google.auth.route')(app);
+require("./routes/google.auth.route")(app);
+require("./routes/billringRoutes")(app);
 
-app.get('/', (req, res) => {
-    res.send({greeting: 'Salut les copains!...'});
+app.get("/", (req, res) => {
+  res.send({ greeting: "Salut les copains!..." });
 });
 
 const PORT = process.env.PORT || 5000;
