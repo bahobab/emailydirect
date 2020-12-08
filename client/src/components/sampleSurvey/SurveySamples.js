78e388209d568a2131fa85003d4b243c76c23554
Konan Houphoue <bahobab@hotmail.com>
2020-12-08 13:25:19 -0600

    updated packages and fixed security issues

---
+++ client/src/components/sampleSurvey/SurveySamples.js
@@ -0,0 +1,3 @@
+// 1. create static sample surveys array
+// 2. create sample survey added to array
+// 3. render sample surveys
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,8 @@
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
+// https://stackoverflow.com/questions/60755768/after-using-this-http-proxy-middleware-it-give-me-this-error-in-reactjs
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    app.use(proxy('/auth/google', {target: 'http://localhost:5000'}));
-    app.use(proxy('/api/**', {target: 'http://localhost:5000'}));
+    app.use(createProxyMiddleware('/auth/google', {target: 'http://localhost:5000'}));
+    app.use(createProxyMiddleware('/api/**', {target: 'http://localhost:5000'}));
 }
